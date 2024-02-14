3743439286cafbcc7a551fc7da6e5dcccd500a01
Laurin49 <welter@diwe.info>
2024-02-14 10:39:12 +0100

    dev02_55-65 Store, Slots, SlotProps ...

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,20 +1,55 @@
 <template>
-  <div class="card">
-    <div class="card-header text-center" role="button">
-      <strong>Montag</strong>
+  <div class="card border-start" :class="cardClasses">
+    <div class="card-header text-center" :class="cardHeaderClasses" role="button">
+      <strong>{{day.fullName}}</strong>
     </div>
     <div class="card-body">
-      <CalendarEvent />
+      <CalendarEvent v-for="event in day.events" :key="event.title" :event="event">
+        <!-- <template v-slot:default="slotProps"> -->
+        <template #default="slotProps">
+          {{ slotProps.event.title }}
+        </template>
+        <template #eventPriority="slotProps">
+          {{ slotProps.priorityDisplayName }}
+        </template>
+      </CalendarEvent>
     </div>
   </div>
 </template>
 <script>
 import CalendarEvent from "./CalendarEvent.vue";
+import Store from "@/store";
 export default {
   name: "CalendarDay",
   components: {
     CalendarEvent,
   },
+  props: {
+    day: {
+      type: Object,
+      required: true,
+      default: function() {
+        return {
+          id: -1,
+          fullName: "Fehlender Wochentag",
+          events: [],
+        }
+      },
+      validator: function(value) {
+        if (Object.keys(value).includes("id")) {
+          return true;
+        }
+      },
+    }
+  },
+  computed: {
+    cardClasses() {
+      return this.day.id === Store.getters.activeDay().id ? ["border-primary"] : null
+    },
+    cardHeaderClasses() {
+      return this.day.id === Store.getters.activeDay().id ? ["bg-primary", "text-white"] : null
+    },
+  }
 };
 </script>
 <style scoped></style>
--- src/components/CalendarWeek.vue
+++ src/components/CalendarWeek.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="calender-week">
     <div class="card-group">
-      <CalendarDay v-for="i in 7" :key="i" />
+      <CalendarDay v-for="day in calendarWeekData" 
+        :key="day.id" :day="day"/>
     </div>
   </div>
 </template>
 <script>
 import CalendarDay from "./CalendarDay.vue";
+import Store from "@/store";
 export default {
   name: "CalendarWeek",
   components: {
     CalendarDay,
+  },
+  data() {
+    return {
+      calendarWeekData: Store.state.calendarWeekData
+    }
   },
 };
 </script>
